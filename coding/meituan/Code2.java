a70d5a5efcc865e3d51bf538be3fd56bb3822e1d
yuyunlong <yuyunlong@vipkid.com.cn>
2020-08-08 16:56:52 +0800

    美团笔试

--- coding/meituan/Code2.java
+++ coding/meituan/Code2.java
@@ -12,11 +12,12 @@ public class Code2 {
     public static void main(String[] args) {
         Scanner  scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[][] array = new int[n][];
-        for (int i = 0; i < n; i++) {
-            array[i][0] = scanner.nextInt();
-            array[i][1] = scanner.nextInt();
-        }
+        int[][] array = new int[n][2];
+
+            for (int i = 0; i < n; i++) {
+                array[i][0] = scanner.nextInt();
+                array[i][1] = scanner.nextInt();
+            }
         handle(array, n);
     }
 
@@ -32,10 +33,11 @@ public class Code2 {
                 settle += (x - y);
             }
             if (x < y) {
-                sumValue += x;
-                settle += x;
+                sumValue += y;
             }
         }
         System.out.println(sumValue + " " + settle);
     }
+
+
 }
