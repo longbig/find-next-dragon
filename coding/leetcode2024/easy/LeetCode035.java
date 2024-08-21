3e7f1f1fe432279723ce81ec1bcf1bf319124b17
longbig <13554698608@163.com>
2024-08-21 09:29:23 +0800

    8月21日提交，解决1 easy 题

---
+++ coding/leetcode2024/easy/LeetCode035.java
@@ -0,0 +1,34 @@
+package leetcode2024.easy;
+
+/**
+ * @author Administrator
+ * @date 2024/8/21 8:58
+ * @description 35. 搜索插入位置
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ */
+public class LeetCode035 {
+
+    /**
+     * 二分查找的思路，注意left == right时还需要判断一次值是大于还是小于
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target <= nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+
+}
